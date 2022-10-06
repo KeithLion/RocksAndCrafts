047859cb1fdb451d00fa2a966d43f4224544ae2c
KeithLion <nortonkiara6@gmail.com>
2022-10-05 22:09:54 -0700

    create main page in views folder and connected that to app.js

---
+++ client/src/views/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = (props) => {
+
+
+    return(
+        <div>
+            <h1>Main Page</h1>
+            <h2>Testing</h2>
+        </div>
+    )
+    
+}
+
+export default Main
